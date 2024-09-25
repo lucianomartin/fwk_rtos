8ef0d4ca379036b0c538a1229dd3b09a25fef089
Luciano Martin <lucianom@xmos.com>
2024-09-25 18:03:13 +0100

    Disable more test steps

--- test/rtos_drivers/hil/hil.cmake
+++ test/rtos_drivers/hil/hil.cmake
@@ -7,7 +7,7 @@ set(GPIO_TEST       0)
 set(SWMEM_TEST      0)
 set(QSPI_FLASH_TEST 0)
 set(I2S_TEST        0)
-set(MIC_ARRAY_TEST  1)
+set(MIC_ARRAY_TEST  0)
 
 #**********************
 # Gather Sources
